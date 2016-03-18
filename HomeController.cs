3ec1a8db6f6fa5f13643f430c1b25a8853697465
vinidimple <vinidimple@yahoo.com>
2016-03-18 08:51:11 +0000

    Added files via upload
    Cosmo\CosmoApp\CosmoApp\HomeController

---
+++ HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+    }
+}
